d131d3b49c032678c6c70a678184cecb963a1354
lwesterl <lauri.v.westerholm@gmail.com>
2019-12-26 22:29:18 +0200

    Adds authentication related functions

--- include/ssh.h
+++ include/ssh.h
@@ -12,6 +12,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #include "str_messages.h"
 
@@ -20,9 +22,34 @@
   *   @brief Store ssh session
   */
 typedef struct {
-  ssh_session session;
-  int sessionID;
+  ssh_session session; /** libssh session */
+  int sessionID; /** libssh session rc */
+  char *message; /** Used to hold info about authetication etc. */
+  unsigned char *hash; /** Remote server public key hash */
+  size_t hash_len;
 } Session;
+
+/**
+  *   @brief Set message for the Sesssion
+  *   @param session Session struct
+  *   @param message New message, possible old message erased
+  */
+void Session_message(Session *session, const char *message);
+
+
+/**
+  *   @enum AuthenticationAction
+  *   @brief Describes authentication action needed
+  */
+enum AuthenticationAction {
+  AUTHENTICATION_OK, /** Authentication completed */
+  AUTHENTICATION_CANCEL, /** User cancelled authentication */
+  AUTHENTICATION_ERROR, /** Error in authentication */
+  AUTHENTICATION_ASK, /** Ask user whether to trust the remote */
+  AUTHENTICATION_ACCEPT, /** User accepts remote public key */
+  AUTHENTICATION_DECLINE /** User declines remote public key */
+};
+
 
 /**
   *   @brief Create a new ssh session
@@ -40,6 +67,28 @@ Session *create_session(const char *username, const char *remote);
   */
 int end_session(Session *session);
 
+/**
+  *   @brief Start authetication using knownhosts
+  *   @param session Session struct, created session
+  *   @return AuthenticationAction needed: AUTHENTICATION_ASK -> ask user authentication
+  *   and call authenticate_key
+  */
+enum AuthenticationAction authenticate_init(Session *session);
 
+/**
+  *   @brief Complete key-based authentication if not finished in authenticate_init
+  *   @param session Created session
+  *   @param action User promted authetication action
+  *   @return AUTHENTICATION_OK, AUTHENTICATION_ERROR or AUTHENTICATION_CANCEL based on
+  *   the user action and authentication procedure
+  */
+enum AuthenticationAction authenticate_key(Session *session, const enum AuthenticationAction action);
+
+/**
+  *   @brief Authenticate using password
+  *   @param session Created Session
+  *   @param password Matching the username
+  */
+enum AuthenticationAction authenticate_password(Session *session, const char *password);
 
 #endif // end SSH_HEADER
--- src/ssh.c
+++ src/ssh.c
@@ -5,10 +5,26 @@
   */
 
 #include "../include/ssh.h"
+#include <libssh/libssh.h>
 
+// Session struct related
+void Session_message(Session *session, const char *message) {
+  if (session) {
+    if (session->message) {
+      free(session->message);
+    }
+    int len = strlen(message) + 1;
+    session->message = malloc(len);
+    strncpy(session->message, message, len);
+  }
+}
+
+// SSH session handling
 Session *create_session(const char *username, const char *remote) {
   if (remote) {
     Session *session = malloc(sizeof(Session));
+    session->message = NULL;
+    session->hash = NULL;
     session->session = ssh_new();
     if (!session->session) {
       perror(get_error(SSH_CREATE_ERROR));
@@ -28,13 +44,144 @@ Session *create_session(const char *username, const char *remote) {
   return NULL;
 }
 
-
 int end_session(Session *session) {
   if (session) {
     ssh_disconnect(session->session);
     ssh_free(session->session);
+    if (session->message) {
+      free(session->message);
+    }
     free(session);
     return 0;
   }
   return -1;
+}
+
+#if LIBSSH_VERSION_MINOR >= 8 || LIBSSH_VERSION_MAJOR > 0
+#define
+  // For newer sshlib versions
+  enum AuthenticationAction authenticate_init(Session *session) {
+    ssh_key pubkey;
+    enum ssh_known_hosts_e state;
+
+    if (ssh_get_server_publickey(session->session, &pubkey) < 0) {
+      Session_message(session, get_error(ERROR_PUB_KEY_NOT_FOUND));
+      return AUTHENTICATION_ERROR;
+    }
+    int ret = ssh_get_publickey_hash(pubkey,
+                               SSH_PUBLICKEY_HASH_SHA1,
+                               &(session->hash),
+                               &(session->hash_len));
+    ssh_key_free(pubkey);
+    if (ret < 0) {
+      Session_message(session, get_error(ERROR_PUB_KEY_HASH_FAILED));
+      return AUTHENTICATION_ERROR;
+    }
+    state = ssh_session_is_known_server(session->session);
+    switch(state) {
+      case SSH_KNOWN_HOSTS_OK:
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_OK;
+      case SSH_KNOWN_HOSTS_CHANGED:
+        Session_message(session, get_error(ERROR_PUB_KEY_CHANGED));
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_ERROR;
+      case SSH_KNOWN_HOSTS_OTHER:
+        Session_message(session, get_error(ERROR_PUB_KEY_NOT_APROPRIATE));
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_ERROR;
+      case SSH_KNOWN_HOSTS_NOT_FOUND:
+        Session_message(session, get_error(KNOWN_HOSTS_NOT_FOUND));
+        return AUTHENTICATION_ASK;
+      case SSH_KNOWN_HOSTS_UNKNOWN:
+        Session_message(session, get_error(SERVER_UNKNOWN));
+        return AUTHENTICATION_ASK;
+      case SSH_KNOWN_HOSTS_ERROR:
+        Session_message(session, ssh_get_error(session->session));
+        return AUTHENTICATION_ERROR;
+    }
+    return AUTHENTICATION_ERROR;
+  }
+
+  enum AuthenticationAction authenticate_key(Session *session, const enum AuthenticationAction action) {
+    // clear public key hash
+    ssh_clean_pubkey_hash(&(session->hash));
+
+    if (action == AUTHENTICATION_ACCEPT) {
+      if (ssh_session_update_known_hosts(session->session) < 0) {
+        Session_message(session, strerror(errno));
+        return AUTHENTICATION_ERROR;
+      }
+      return AUTHENTICATION_OK;
+    }
+    // user has decided to decline the server public key
+    return AUTHENTICATION_CANCEL;
+  }
+#else
+  // For old sshlib versions
+  enum AuthenticationAction authenticate_init(Session *session) {
+    ssh_key pubkey;
+    enum ssh_server_known_e state;
+
+    if (ssh_get_publickey(session->session, &pubkey) < 0) {
+      Session_message(session, get_error(ERROR_PUB_KEY_NOT_FOUND));
+      return AUTHENTICATION_ERROR;
+    }
+    int ret = ssh_get_publickey_hash(pubkey,
+                               SSH_PUBLICKEY_HASH_SHA1,
+                               &(session->hash),
+                               &(session->hash_len));
+    ssh_key_free(pubkey);
+    if (ret < 0) {
+      Session_message(session, get_error(ERROR_PUB_KEY_HASH_FAILED));
+      return AUTHENTICATION_ERROR;
+    }
+    state = ssh_is_server_known(session->session);
+    switch(state) {
+      case SSH_SERVER_KNOWN_OK:
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_OK;
+      case SSH_SERVER_KNOWN_CHANGED:
+        Session_message(session, get_error(ERROR_PUB_KEY_CHANGED));
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_ERROR;
+      case SSH_SERVER_FOUND_OTHER:
+        Session_message(session, get_error(ERROR_PUB_KEY_NOT_APROPRIATE));
+        ssh_clean_pubkey_hash(&(session->hash));
+        return AUTHENTICATION_ERROR;
+      case SSH_SERVER_FILE_NOT_FOUND:
+        Session_message(session, get_error(KNOWN_HOSTS_NOT_FOUND));
+        return AUTHENTICATION_ASK;
+      case SSH_SERVER_NOT_KNOWN:
+        Session_message(session, get_error(SERVER_UNKNOWN));
+        return AUTHENTICATION_ASK;
+      case SSH_SERVER_ERROR:
+        Session_message(session, ssh_get_error(session->session));
+        return AUTHENTICATION_ERROR;
+    }
+    return AUTHENTICATION_ERROR;
+  }
+
+  enum AuthenticationAction authenticate_key(Session *session, const enum AuthenticationAction action) {
+  // clear public key hash
+  ssh_clean_pubkey_hash(&(session->hash));
+
+  if (action == AUTHENTICATION_ACCEPT) {
+    if (ssh_write_knownhost(session->session) < 0) {
+      Session_message(session, strerror(errno));
+      return AUTHENTICATION_ERROR;
+    }
+    return AUTHENTICATION_OK;
+  }
+  // user has decided to decline the server public key
+  return AUTHENTICATION_CANCEL;
+  }
+#endif // authenticate_init and authenticate_key implementations
+
+enum AuthenticationAction authenticate_password(Session *session, const char *password) {
+  if (ssh_userauth_password(session->session, NULL, password) != SSH_AUTH_SUCCESS) {
+    Session_message(session, get_error(ERROR_PASSWORD_AUTHENTICATION));
+    return AUTHENTICATION_ERROR;
+  }
+  return AUTHENTICATION_OK;
 }
