d6aa952105ae7cb9b723e3f52a79f1472779c590
lwesterl <lauri.v.westerholm@gmail.com>
2019-12-31 17:18:49 +0200

    Modify to start the UI event loop

--- src/main.c
+++ src/main.c
@@ -4,17 +4,25 @@
   *   @brief FileManager main
   */
 
-#include "../include/ssh.h"
+#include "../include/UI.h"
 
-int main() {
-  const char *username = "";
+int main(int argc, char *argv[]) {
+
+  initUI(argc, argv);
+
+  /*const char *username = "test";
   const char *address = "localhost";
 
   Session *session = create_session(username, address);
 
   if (session) {
     printf("SessionID: %d\n", session->sessionID);
+
+    enum AuthenticationAction result = authenticate_init(session);
+    printf("Authentication result: %d\n", result);
+
     printf("Removing session\n");
     end_session(session);
-  }
+  }*/
+  return EXIT_SUCCESS;
 }
