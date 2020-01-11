b4882e6db479fab26e7b588e1f1478353bb8cc28
lwesterl <lauri.v.westerholm@gmail.com>
2020-01-12 00:43:27 +0200

    Modify test_fs.c to test fs_rename and recursive fs_rmdir

--- test/test_fs.c
+++ test/test_fs.c
@@ -17,6 +17,7 @@ void print_File(File_t *file, __attribute__((unused)) void *ptr) {
 
 int main() {
   const char *dir_name = "testDIR";
+  const char *dir_name2 = "testDIR2";
   assert(fs_mkdir(dir_name) == FILE_WRITTEN_SUCCESSFULLY);
   assert(file_exists(dir_name));
   assert(!file_exists("some_random_file_name"));
@@ -25,8 +26,20 @@ int main() {
   assert((files = ls_dir(files, ".")));
   printf("files length: %d\n", g_slist_length(files));
   iterate_FileList(files, print_File, NULL);
+  clear_Filelist(files);
 
-  assert(fs_rmdir(dir_name) == 0);
+  const char *file = "testDIR/test_file.txt";
+  const char *file2 = "testDIR/test_file_updated.txt";
+  int fd = open(file, O_CREAT | O_WRONLY);
+  if (fd) {
+    const char *buff = "Hello world\n";
+    write(fd, buff, strlen(buff));
+    close(fd);
+    assert(fs_rename(file, file2) == FILE_WRITTEN_SUCCESSFULLY);
+  }
+
+  assert(fs_rename(dir_name, dir_name2) == FILE_WRITTEN_SUCCESSFULLY);
+  assert(fs_rmdir(dir_name2, true) == 0);
 
   printf("test_fs.c successfully finished\n");
   return EXIT_SUCCESS;
